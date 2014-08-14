a0de2e2807cb0cb89bc6736dfd2482f046be7905
Zander Baldwin <mynameiszanders@gmail.com>
2014-08-14 16:54:38 +0100

    Update Main Transport

--- src/Transport/Http.php
+++ src/Transport/Http.php
@@ -4,6 +4,8 @@
 
     use Nosco\Cashflows\TransportInterface;
     use GuzzleHttp\Client as Guzzle;
+    use Nosco\Cashflows\Exceptions;
+    use Nosco\Cashflows\Response\Authorised as AuthorisedResponse;
 
     class Http implements TransportInterface
     {
@@ -36,7 +38,43 @@
          */
         public function send($url, array $fields)
         {
-            var_dump(func_get_args());
+            $post = self::$guzzle->createRequest('POST', $url, ['body' => $fields]);
+            try {
+                $response = $self::$guzzle->send($post);
+            }
+            catch(GuzzleHttp\Exception\TransferException $e) {
+                throw new TransportException('The transport adapter encountered an error: "' . $e->getMessage() . '"', $e->getCode(), $e);
+            }
+            $responseParts = preg_split('/\\s*\\\\|\\s*/', (string) $response->getBody(), -1, PREG_SPLIT_NO_EMPTY);
+            if(is_array($responseParts) && count($responseParts) === 5) {
+                switch($responseParts[0]) {
+                    case 'A':
+                        break;
+                    case 'B':
+                        throw new Exceptions\Response\NotAuthorised\BlockedException($responseParts[1], $responseParts[2], $responseParts[3], $responseParts[4]);
+                        break;
+                    case 'C':
+                        throw new Exceptions\Response\NotAuthorised\CancelledException($responseParts[1], $responseParts[2], $responseParts[3], $responseParts[4]);
+                        break;
+                    case 'D':
+                    case 'R':
+                        throw new Exceptions\Response\NotAuthorised\DeclinedException($responseParts[1], $responseParts[2], $responseParts[3], $responseParts[4]);
+                        break;
+                    case 'V':
+                        throw new Exceptions\Response\InvalidRequestException($responseParts[1], $responseParts[2], $responseParts[3], $responseParts[4]);
+                        break;
+                    case 'S':
+                        throw new Exceptions\CashflowsSystemException;
+                        break;
+                    default:
+                        throw new Exceptions\Response\InvalidResponseException;
+                        break;
+                }
+                return new AuthorisedResponse($responseParts[1], $responseParts[2], $responseParts[3]);
+            }
+            else {
+                throw new Exceptions\Response\InvalidResponseException;
+            }
         }
 
     }
